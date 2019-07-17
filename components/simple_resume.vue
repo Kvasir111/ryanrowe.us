ce5edbd6130da4020491aae3ff0daeb5c1602a31
varangian111 <rrowe222@gmail.com>
2019-07-17 16:27:54 -0500

    added skills list

--- components/simple_resume.vue
+++ components/simple_resume.vue
@@ -1,22 +1,30 @@
 <template>
 	<div>
-	<div id="basic-information" class="text-center text-white">
-		<h1 class="text-2xl permanantMarker">{{name}}</h1>
-		<h2 class="text-xl permanantMarker">{{myTitle}}</h2>
-	</div>
+		<div class="text-center text-white" id="basic-information">
+			<h1 class="text-2xl permanentMarker">{{name}}</h1>
+			<h2 class="text-xl permanentMarker">{{myTitle}}</h2>
+		</div>
 		<div class="text-white mx-auto mt-2" id="job-experience">
-			<h1 class="text-center border-b-2 w-1/4 mx-auto"> Job Experience </h1>
+			<h1 class="resume-heading"> Job Experience </h1>
 			<ul class="mx-auto w-1/4 text-center">
-				<li :key="index" class="text-left job-text" v-bind:value="jobs.years" v-for="(jobs, index) in jobs"><u>{{jobs.years}}</u>
+				<li :key="index" class="job-text" v-bind:value="jobs.years" v-for="(jobs, index) in jobs"><u>{{jobs.years}}</u>
 					<br> {{jobs.company}} {{jobs.job}}
 				</li>
 			</ul>
 		</div>
 		<div class="text-white mx-auto mt-2" id="education">
-			<h1 class="text-center border-b-2 w-1/4 mx-auto">Education</h1>
+			<h1 class="resume-heading">Education</h1>
 			<ul class="mx-auto w-1/4 text-center">
-				<li :key="index" :value="education.institution" v-for="(education , index) in education">
+				<li :key="index" :value="education.institution" class="job-text"
+				    v-for="(education , index) in education">
 					{{education.institution}} <br>{{education.degree}}
+				</li>
+			</ul>
+		</div>
+		<div class="text-white mx-auto mt-2" id="skills">
+			<h1 class="resume-heading">Skills</h1>
+			<ul class="mx-auto w-1/4 text-center">
+				<li :key="index" :value="skills" class="job-text" v-for="(skills, index) in skills">{{skills.skill}}
 				</li>
 			</ul>
 		</div>
@@ -27,24 +35,24 @@
 	export default {
 		name: "simple_resume",
 		data: function () {
-			return{
-				name : "Ryan Rowe",
+			return {
+				name: "Ryan Rowe",
 				myTitle: "Developer | Combat Engineer | Powerlifter",
-				jobs:[
-					{years: "2012-2014" , company: "Best Buy" ,job: "PC Sales Consultant"},
-					{years: "2014-2016" , company: "Geek Squad", job: "Advanced Repair Agent"},
-					{years: "2016-2022" , company: "Wisconsin Army National Guard", job: "Combat Engineer (12B)"},
-					{years: "2018-2020" , company : "Hiawatha Broadband Communications", job: "TAC Analyst"},
+				jobs: [
+					{years: "2012-2014", company: "Best Buy", job: "PC Sales Consultant"},
+					{years: "2014-2016", company: "Geek Squad", job: "Advanced Repair Agent"},
+					{years: "2016-2022", company: "Wisconsin Army National Guard", job: "Combat Engineer (12B)"},
+					{years: "2018-2020", company: "Hiawatha Broadband Communications", job: "TAC Analyst"},
 				],
-				education :[
-					{institution : "Dakota County Technical College", degree: "AAS Networking Administration"},
-					{institution : "Winona State University", degree: "BAS Computer Science, Focus in Bioinfomatics"},
+				education: [
+					{institution: "Dakota County Technical College", degree: "AAS Networking Administration"},
+					{institution: "Winona State University", degree: "BAS Computer Science, Focus in Bioinfomatics"},
 				],
-				skills : [
+				skills: [
 					{skill: "Java Programming"},
 					{skill: "C Programming"},
 					{skill: "Python Programming"},
-					{skill : "Nuxt/Vue.js"},
+					{skill: "Nuxt/Vue.js"},
 					{skill: "Windows Server"},
 					{skill: "Cisco Networking"}
 				]
