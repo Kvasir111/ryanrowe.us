0e8c9838543573aaa534cffe91afd042dfca856d
varangian111 <rrowe222@gmail.com>
2019-07-17 16:34:04 -0500

    setting up projects page v

---
+++ pages/projects.vue
@@ -0,0 +1,48 @@
+<template>
+	<div id="github" v-on:load="getGit">
+		<ul id="github-links"></ul>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "projects",
+		methods:{
+			getGit(){
+				$.ajax({
+					url: "https://api.github.com/users/varangian111/repos",
+					jsonp: true,
+					method: "GET",
+					dataType: "json",
+					success: function(repos) {
+						let repoList = Object.keys(repos).length;
+						for (let i = 0 ; i < repoList ; i++){
+
+							let repoName = repos[i].name;
+							let description = repos[i].description;
+							let url = repos[i].html_url;
+
+							if (description === null){
+								description = "No Description Provided"
+							}
+							console.log(repoName);
+							$("#github-links").append(
+								'<a target="_blank" href="'+url+'" rel="noreferrer" class="card text-left shadow-none sm:shadow-md inline-block w-full sm:w-4/5 md:w-2/3 comfort-light hover:shadow-lg">' +
+								'<h3 class="text-left text-xl text-red-600 comfort pb-5">' +
+								repoName +
+								'</h3>' +
+								description +
+								'</a>' +
+								'<br>'
+							);
+						}
+					}
+				});
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
