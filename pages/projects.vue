2d7b70491f1f7173364dce65a2edeb3fda7addd1
varangian111 <rrowe222@gmail.com>
2019-07-23 16:52:37 -0500

    well the projects page works now, it looks like ass because I couldn't be bothered today

--- pages/projects.vue
+++ pages/projects.vue
@@ -5,10 +5,15 @@
 </template>
 
 <script>
+	import $ from 'jquery';
 	export default {
 		name: "projects",
+		mounted : function (){
+			this.getGit();
+		},
 		methods:{
 			getGit(){
+				console.log("KEK");
 				$.ajax({
 					url: "https://api.github.com/users/varangian111/repos",
 					jsonp: true,
@@ -27,12 +32,14 @@
 							}
 							console.log(repoName);
 							$("#github-links").append(
+								'<li>' +
 								'<a target="_blank" href="'+url+'" rel="noreferrer" class="card text-left shadow-none sm:shadow-md inline-block w-full sm:w-4/5 md:w-2/3 comfort-light hover:shadow-lg">' +
 								'<h3 class="text-left text-xl text-red-600 comfort pb-5">' +
 								repoName +
 								'</h3>' +
 								description +
 								'</a>' +
+							    '</li>' +
 								'<br>'
 							);
 						}
