a42b577d4c462cc8a2cac64a1a88130ca2819917
varangian111 <rrowe222@gmail.com>
2019-07-25 10:07:06 -0500

    moved fonts to static directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 // See default config https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 module.exports = {
-  theme: {
-    extend: {
-      colors:{
-        nuBlack: '#1a1a1d',
-        nuGrey: '#4e4e50',
-        nuPinkDark: '#6f2232',
-        nuPink: '#950740',
-        nuPinkLight: '#c3073f'
-      },
-    }
-  },
-  variants: {}
+	theme: {
+		extend: {
+			fontFamily:{
+
+			},
+			colors:{
+				nuBlack: '#1a1a1d',
+				nuGrey: '#4e4e50',
+				nuPinkDark: '#6f2232',
+				nuPink: '#950740',
+				nuPinkLight: '#c3073f'
+			},
+		}
+	},
+	variants: {}
 }
