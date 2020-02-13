8832c7e1a54e58592663b5869c95893fef5233ae
Ryan Rowe <rrowe222@gmail.com>
2020-02-13 13:05:21 -0600

    Decided it was best to only show the apps that I've actually deployed in some capacity

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="h-screen flex">
 	<div class="m-auto " id="github">
-		<h1 class="text-2xl border-b-2 w-1/3 mb-4 text-center mx-auto border-b-white text-center text-white">Github Repos</h1>
+		<h1 class="text-2xl border-b-2 w-1/3 mb-4 text-center mx-auto border-b-white text-center text-white">Deployed Apps</h1>
 		<ul class="mx-auto text-center">
 			<repo-node :key=index v-for="(link, index) in gitLinks"
 			           v-bind:repo-name="gitLinks[index].repoName"
@@ -32,16 +32,17 @@
 			axios.get("https://api.github.com/users/Kvasir111/repos").then(response =>
 				{
 					for (let index in response.data){
-						console.log(response.data[index].name);
-						console.log(response.data[index].html_url);
-						this.gitLinks.push(
-							{
-								repoName : response.data[index].name,
-								repoLink : response.data[index].html_url,
-								repoLang : response.data[index].language,
-								repoDescription: response.data[index].description
-							}
-						)
+						if (response.data[index].homepage != null && response.data[index].homepage !== ""){
+							this.gitLinks.push(
+									{
+										repoName : response.data[index].name,
+										repoLink : response.data[index].homepage,
+										repoLang : response.data[index].language,
+										repoDescription: response.data[index].description
+									}
+							)
+						}
+
 					}
 				}
 			);
