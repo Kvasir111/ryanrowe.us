7b6481b69f3d9e0b71d34015411d7af6ba111cea
Kvasir4117 <rrowe222@gmail.com>
2021-04-01 17:26:10 -0500

    Updated "About.vue" with the "my story" paragraph

--- components/about.vue
+++ components/about.vue
@@ -4,15 +4,33 @@
     <p>
       My name is Ryan! <br>
       Now, you are probably hoping this section will contain some sort of useful title like "Hello my name is Ryan Rowe,
-      I am a Java Engineer" <br>
-      or "Hello, yes, I am a .NET developer", but those one line titles I don't think really do a great job at giving
-      anyone an accurate picture <br>
-      of who somebody is. Instead I'm hoping this portfolio will give you an overview of what I've done, what my
-      interests are, and what I hope
-      the future holds for me!
+      I am a Java Engineer" or "Hello, yes, I am a .NET developer", but those one line titles I don't think really do a
+      great job at giving anyone an accurate picture of who somebody is. Instead I'm hoping this portfolio will give you
+      an overview of what I've done, what my interests are, and what I hope the future holds for me!
     </p>
-    <h2>My Story</h2>
+    <h2 class="text-xl">My Story</h2>
     <p>
+      When I left high school and stepped out from the borders of safety of mandatory education, I had absolutely no
+      idea what I wanted to do with my life. I knew I liked technology, and messing around on computers was fun, but
+      what actual application that passion and aptitude had professionally, did not actually click with me. In my
+      indecision, I decided to at least start my education with a technical skill, something concrete that I could say
+      “I did”, so I went and got a degree at my local technical college. After two years, I graduated, and realized that
+      I had more than just a passing shine for technology and computers and decided to pursue even more education. I
+      then went and got myself a cool four-year degree at a state college and just kind of hoped that I would find where
+      I belonged when I got done there.
+      Much to nobody’s surprise but my own, I didn’t find my place. What my education gave me was a large pool of
+      information, but no actual expertise or practical experience in one area. Which, I guess is the point of higher
+      education. It really upsets me when people say that higher education did nothing for them when so many jobs, the
+      things we do day in day out so we can feed ourselves, just straight up require a form of college now to even get
+      in the lobby, but I do understand why that sentiment exists now. I know how to code, I know how a computer works,
+      I know how computers talk to each other. I have a nice bird’s eye view of the landscape of computer science, but
+      the actual texture of the ground is still out of my reach. A degree is just your ticket to ride the air balloon
+      over the peaks and valleys of the fields of science, your passion for one area is where you say goodbye to
+      academia and take a leap of faith out of the balloon, onto the harsh unforgiving earth below.
+      All of that long winded, flowery metaphor is to say, is that over six years of higher education, multiple jobs,
+      arguments with professors, and lectern thumping class speeches, I still have not found my place. And I have
+      settled on that being OK. The fatalism of predestination isn’t what I want, and just because you didn’t find your
+      slot in the great wheel of humanity right away, doesn’t mean you’re a failure.
 
     </p>
   </div>
