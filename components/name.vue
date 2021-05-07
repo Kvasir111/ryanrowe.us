e99ba49b38ed5741b5c9c66794b88e8c426f533c
Kvasir4117 <rrowe222@gmail.com>
2021-05-07 16:07:54 -0500

    Testing out some new text effects, reverted them for now

--- components/name.vue
+++ components/name.vue
@@ -1,17 +1,21 @@
 <template>
-  <div>
-    <h1 class="text-3xl">Hi ;} I'm Ryan <br> I /fP/? Computers</h1>
+  <div class="w-screen">
+    <h1 class="text-4xl">Name: Ryan Rowe</h1>
+    <h1 class="text-3xl">Occupation: Computer Man</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: "name"
+  name: "name",
 }
+
 </script>
 
 <style scoped>
 h1 {
+  font-family: "Hacked CRT", "sans-serif";
   color: #f44268;
 }
+
 </style>
